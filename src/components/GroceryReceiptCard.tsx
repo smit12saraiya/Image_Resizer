fde09135c4d4c4dcbaa6a9a8c520513246db8b6c
Smit Saraiya <smit12saraiya@gmail.com>
2025-11-06 19:12:12 -0500

    Updated GroceryReceiptCard.tsx

--- src/components/GroceryReceiptCard.tsx
+++ src/components/GroceryReceiptCard.tsx
@@ -36,8 +36,8 @@ export function GroceryReceiptCard({ expense, onDelete, showDelete = false }: Gr
   } catch {
     items = rawData?.items || [];
   }
-  console.log('Expense', JSON.parse(expense))
-  console.log('Items', JSON.parse(expense.items))
+  console.log('Expense', expense))
+  console.log('Items', expense.items)
   const totalItemsCount = rawData?.total_items_count || items.length;
 
   return (
